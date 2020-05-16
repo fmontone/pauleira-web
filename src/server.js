428d3d1ca6dc2065e5081e71dd8bb63751471888
Fabio Montone <montone@gmail.com>
2020-05-15 21:49:19 -0300

    Umbler Deploy / Install / Build process

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+const config = {
+  folder: 'build',
+  forceHttps: process.env.NODE_ENV !== 'development',
+  port: process.env.PORT || 3333,
+};
+
+if (config.forcarHTTPS)
+  // Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
+  app.use((req, res, next) => {
+    // Cria um middleware onde todas as requests passam por ele
+    if (req.headers['x-forwarded-proto'] === 'http')
+      // Checa se o protocolo informado nos headers é HTTP
+      res.redirect(`https://${req.headers.host}${req.url}`);
+    // Redireciona pra HTTPS
+    // Se a requisição já é HTTPS
+    else next(); // Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+  });
+
+app.use(express.static(config.folder)); // Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
+
+app.get('*', (req, res) => {
+  // O wildcard '*' serve para servir o mesmo index.html independente do folder especificado pelo navegador.
+  res.sendFile(path.join(__dirname, config.folder, 'index.html'));
+});
+
+app.listen(config.port, () => {
+  console.log(`Pauleira Front Web Listening ${config.port}!`); /* eslint-disable-line */
+});
