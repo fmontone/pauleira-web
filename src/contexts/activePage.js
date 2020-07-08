0a7df34f0e88ea51ada34c2a45f16aea10ba7319
Fabio Montone <montone@gmail.com>
2020-07-08 20:13:50 -0300

    Update: activePage

--- src/contexts/activePage.js
+++ src/contexts/activePage.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const ActivePageContext = createContext(null);
