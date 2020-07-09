695cf68feee2bb0aad21ae5a0fed55944bcf1234
Fabio Montone <montone@gmail.com>
2020-07-09 16:01:43 -0300

    Test: NODE_ENV

--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// ALERT
-alert(process.env.NODE_ENV);
-
 ReactDOM.render(<App />, document.getElementById('root'));
